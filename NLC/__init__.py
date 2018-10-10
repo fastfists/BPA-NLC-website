813dbc8b1a35326609f6b6b917fbcfef2b6bb9f8
fastfists <fdenzell@gmail.com>
2018-10-10 18:51:14 -0500

    added a models package

--- NLC/__init__.py
+++ NLC/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 def create_app():
-    from . import routes
+    from . import routes, models
     app = Flask(__name__)
     routes.init_app(app)
+    models.init_app(app)
     return app
