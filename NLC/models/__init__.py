af12d59a8a95938b868c7b5aafd9c51b60fb913e
Denzell Ford <fdenzell@gmail.com>
2018-10-10 23:49:20 -0500

    Began base database

--- NLC/models/__init__.py
+++ NLC/models/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from os import environ
+from .base import db
+from . import users
 
 def init_app(app:Flask):
-    pass
+    app.config["SQLALCHEMY_DATABASE_URI"] = environ["CLEARDB_DATABASE_URL"]
+    db.init_app(app)
