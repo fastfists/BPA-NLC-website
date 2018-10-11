71e5894a42c119243cd048560e86815a1705a209
Denzell Ford <fdenzell@gmail.com>
2018-10-11 07:50:26 -0500

    added db

--- NLC/models/__init__.py
+++ NLC/models/__init__.py
@@ -4,5 +4,5 @@ from .base import db
 from . import users
 
 def init_app(app:Flask):
-    app.config["SQLALCHEMY_DATABASE_URI"] = environ["CLEARDB_DATABASE_URL"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://bccfea76054235:cf209ee2@us-cdbr-iron-east-01.cleardb.net/heroku_3a392d53d178674?reconnect=true" #TODO: Export to envionment variable
     db.init_app(app)
