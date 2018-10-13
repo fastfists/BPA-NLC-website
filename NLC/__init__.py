737a55835fbe0df5568c62d29fd59723d03d5e7f
Denzell Ford <fdenzell@gmail.com>
2018-10-13 11:27:46 -0500

    added db for testing

--- NLC/__init__.py
+++ NLC/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    from . import routes, models
+    from NLC import routes, models
     app = Flask(__name__)
     routes.init_app(app)
     models.init_app(app)
