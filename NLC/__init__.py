525a3ce54469bf5b6ddb7fd04a608a4857772618
fastfists <fdenzell@gmail.com>
2018-10-14 10:53:54 -0500

    oops import errors

--- NLC/__init__.py
+++ NLC/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app() -> Flask:
-    from NLC import routes, models
+    from . import routes, models
     app = Flask(__name__)
     routes.init_app(app)
     models.init_app(app)
--- NLC/models/__init__.py
+++ NLC/models/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from os import environ
 from .base import db
-from models import user
+from . import user
 
 def init_app(app:Flask):
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
     db.init_app(app)
     db.app = app
+    db.create_all()
